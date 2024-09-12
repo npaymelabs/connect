be32234e042290b3c34ebedddd1d1f037edacb5c
Gustavo <gustavo@obex.pw>
2024-09-12 20:16:27 +0100

    try to fix coinbase wallet with nextjs

--- lib/components/smartwallet/CreateWalletButton.tsx
+++ lib/components/smartwallet/CreateWalletButton.tsx
@@ -1,32 +1,8 @@
-import { useCallback } from "react";
+import { useCallback, useMemo } from "react";
 import { CoinbaseWalletSDK } from "@coinbase/wallet-sdk";
 import { CoinbaseWalletLogo } from "./CoinbaseWalletLogo";
 import { BrandedProgrammeButton } from "../components/Buttons";
-
-// const buttonStyles = {
-//   background: 'transparent',
-//   border: '1px solid transparent',
-//   boxSizing: 'border-box',
-//   display: 'flex',
-//   alignItems: 'center',
-//   justifyContent: 'space-between',
-//   // width: 100%,
-//   // fontFamily: 'Arial, sans-serif',
-//   // fontWeight: 'bold',
-//   // fontSize: 18,
-//   backgroundColor: '#0052FF',
-//   paddingLeft: 15,
-//   paddingRight: 30,
-//   borderRadius: 10
-// }
-
-const sdk = new CoinbaseWalletSDK({
-  appName: "My Dapp",
-  appLogoUrl: "https://example.com/logo.png",
-  appChainIds: [84532],
-});
-
-const provider = sdk.makeWeb3Provider();
+import { Address } from "viem";
 
 export function CreateWalletButton({
   handleSuccess,
@@ -35,21 +11,21 @@ export function CreateWalletButton({
   handleSuccess: (address: string) => void;
   handleError: (error: unknown) => void;
 }) {
-  // const { connectors, connect, data } = useConnect()
+  const provider = useMemo(() => {
+    const sdk = new CoinbaseWalletSDK({
+      appName: "My Dapp",
+      appLogoUrl: "https://example.com/logo.png",
+      appChainIds: [84532],
+    });
 
-  // const createWallet = useCallback(() => {
-  //   const coinbaseWalletConnector = connectors.find(
-  //     (connector) => connector.id === 'coinbaseWalletSDK'
-  //   )
-  //   if (coinbaseWalletConnector) {
-  //     connect({ connector: coinbaseWalletConnector })
-  //   }
-  // }, [connectors, connect])
+    const provider = sdk.makeWeb3Provider();
+
+    return provider;
+  }, []);
 
   const createWallet = useCallback(async () => {
     try {
-      // @ts-ignore
-      const [address] = await provider.request({
+      const [address] = await provider.request<[Address]>({
         method: "eth_requestAccounts",
       });
       handleSuccess(address);
